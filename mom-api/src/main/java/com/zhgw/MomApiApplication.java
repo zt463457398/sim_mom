eb0aab47bfca4d4d9a4c80608445889de65d2258
zt463457398 <847503085@qq.com>
2024-11-21 16:11:33 +0800

    项目完善结构

---
+++ mom-api/src/main/java/com/zhgw/MomApiApplication.java
@@ -0,0 +1,15 @@
+package com.zhgw;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.zhgw.mapper")
+public class MomApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomApiApplication.class, args);
+    }
+
+}
---
+++ mom-api/src/test/java/com/zhgw/MomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
