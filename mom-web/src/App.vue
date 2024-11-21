261bebceb2637a3a5fc722cd3e838606b13bdf65
zt463457398 <847503085@qq.com>
2024-11-21 14:51:20 +0800

    重置前端代码

--- mom-web/src/App.vue
+++ mom-web/src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <router-view />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script setup lang="ts">
-// 使用TypeScript
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
 #app {
-  font-family: Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  height: 100vh;
-}
-
-/* 重置一些基础样式 */
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-  padding: 0;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- mom-web/vue.config.js
+++ mom-web/vue.config.js
@@ -1,22 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      }
-    }
-  },
-  pages: {
-    index: {
-      entry: 'src/main.ts',
-      template: 'public/index.html',
-      filename: 'index.html',
-      title: 'MOM制造运营管理系统'
-    }
-  }
+  transpileDependencies: true
 })
